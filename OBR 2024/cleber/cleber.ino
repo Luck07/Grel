dff587a516bf4305b17a932fa2bdaf3f56588687
Luck07 <airameliel@gmail.com>
2024-06-20 16:17:44 -0300

    cleber

--- OBR 2024/cleber/cleber.ino
+++ OBR 2024/cleber/cleber.ino
@@ -1,142 +1,106 @@
 // Switch
-#include "definir.h" // Dando include nas variaveis e funções
+#include "definir.h"  // Dando include nas variaveis e funções
 
 // Usando array para colocar todos os pinos, coloquei os sensores em uma certa posição por causa do BitSwift em baixo
-const int pinos[] = {s_oeste, s_noroeste, s_norte, s_nordeste, s_leste};
+const int pinos[] = { s_esq, s_mesq, s_m, s_mdir, s_dir };
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-    // Colocando os sensores como INPUT, e o resto como OUTPUT, tudo isso pelo array
-    for (int i = 0; i < 5; i++) // Usando o array para fazer os pinmode como input
-        pinMode(pinos[i], INPUT);
+  // Colocando os sensores como INPUT, e o resto como OUTPUT, tudo isso pelo array
+  for (int i = 0; i < 5; i++)  // Usando o array para fazer os pinmode como input
+    pinMode(pinos[i], INPUT);
 
   serv_esq.attach(servo_esquerda);
   serv_dir.attach(servo_direita);
 }
-void loop()
-{
+void loop() {
   //calibra();
   //Serial.print(leitura, BIN);
-  Serial.print(analogRead(s_noroeste));
-  Serial.print(" / ");
-  Serial.println(analogRead(s_nordeste));
+  bool besq = map(analogRead(s_esq), preto_esq, branco_esq, 0, 1023) >= 500 ? 1 : 0;
+  bool bmesq = map(analogRead(s_mesq), preto_mesq, branco_mesq, 0, 1023) >= 500 ? 1 : 0;
+  bool bm = map(analogRead(s_m), preto_m, branco_m, 0, 1023) >= 500 ? 1 : 0;
+  bool bmdir = map(analogRead(s_mdir), preto_mdir, branco_mdir, 0, 1023) >= 500 ? 1 : 0;
+  bool bdir = map(analogRead(s_dir), preto_dir, branco_dir, 0, 1023) >= 500 ? 1 : 0;
 
-  if ((analogRead(s_noroeste) <= max_noroeste) && (analogRead(s_nordeste) >= max_nordeste )) //! Fazer micro ajuste para esquerda
+  if((besq && bmesq && bm && bmdir && bdir) || (besq && bmesq && !bm && bmdir && bdir) || (besq && !bmesq && !bm && !bmdir && bdir) || (!besq && !bmesq && !bm && !bmdir && !bdir))
   {
-    if (ver == false)
-      {
-          vel_esquerda();
-          Serial.println("Mini esquerda");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
+    vel_frente();
   }
-  else if ((analogRead(s_noroeste) >= max_noroeste) && (analogRead(s_nordeste) <= max_nordeste)) //! Fazer micro ajuste para direita
+  else if((besq && !bmesq && bm && bmdir && bdir))
   {
-    if (ver == false)
-    {
-      vel_direita();
-      Serial.println("Mini direita");
-    }
-    else
-    {
-      vel_re();
+    vel_direita();
+  }
+  else if((besq && bmesq && bm && !bmdir && bdir))
+  {
+    vel_esquerda();
+  }
+  else if((!besq && bmesq && bm && bmdir && bdir) || (!besq && !bmesq && !bm && !bmdir && bdir) || (!besq && bmesq && !bm && bmdir && bdir) || (!besq && !bmesq && bm && !bmdir && bdir))
+  {
+    //esq_90();
+    //vel_parar(3000);
+  }
+  else if((besq && bmesq && bm && bmdir && !bdir) || (besq && !bmesq && !bm && !bmdir && !bdir) || (besq && bmesq && !bm && bmdir && !bdir) || (besq && !bmesq && bm && !bmdir && !bdir))
+  {
+    dir_90();
+    //vel_parar(3000);
+  }
+  /* if ((analogRead(s_mesq) >= media_mesq) && (analogRead(s_mdir) <= media_mdir))  //! Fazer micro ajuste para esquerda
+  {
+    if (ver == false) {
+      //vel_esquerda();
+      Serial.println("Mini esquerda");
+    } else {
+      //vel_re();
       delay(delay_pas);
       ver = false;
     }
-  }
-  else
+  } else if ((analogRead(s_mesq) <= media_mesq) && (analogRead(s_mdir) >= media_mdir))  //! Fazer micro ajuste para direita
   {
-    vel_frente();
-    Serial.println("Frente");
-  }
-  delay(100);
-  vel_parar(1000);
-  
-  // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
-  // Alguns nao tem break; porque faz a mesma coisa
-  /* switch (leitura)
-  {
-  case 0b1111:
-  case 0b0000:
-  case 0b0110: //! Caso de ele ir so pra frente
-      if (ver == false)
-      {
-          vel_frente();
-          Serial.println(" / Frente");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
-      break;
-  case 0b0010: //! Caso dele fazer micro ajuste para direita
-      if (ver == false)
-      {
-          vel_direita();
-          Serial.println(" / Mini direita");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
-      break;
-  case 0b0100: //! Caso dele fazer micro ajuste para esquerda
-      if (ver == false)
-      {
-          vel_esquerda();
-          Serial.println(" / Mini esquerda");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
-      break;
-  case 0b1000:
-  //case 0b1100:
-  case 0b1110:
-  //case 0b1010: //! Casos de fazer o esquerda 90  
-      if (ver == false)
-      {
-          vel_esquerda();
-          Serial.println(" / Mini esquerda");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
-      break;
-  case 0b0001:
-  //case 0b0011:
-  case 0b0111:
-  //case 0b0101: //! Casos de fazer o direita 90
-      if (ver == false)
-      {
-          vel_direita();
-          Serial.println(" / Mini direita");
-      }
-      else
-      {
-          vel_re();
-          delay(delay_pas);
-          ver = false;
-      }
-      break;
-  default:
-      break;
-  } */
-  Serial.println();
+    if (ver == false) {
+      //vel_direita();
+      Serial.println("Mini direita");
+    } else {
+      //vel_re();
+      delay(delay_pas);
+      ver = false;
+    }
+  } else { */
+
+    // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
+    // Alguns nao tem break; porque faz a mesma coisa
+    /* switch (leitura) {
+      case 0b0110:
+      case 0b1111:
+      case 0b0000:  //! Caso de ele ir so pra frente
+        vel_frente();
+        Serial.println(" / Frente");
+        break;
+      case 0b0100:  //! Casos de fazer mini esquerda
+        Serial.println(" / mini esquerda");
+        vel_esquerda();
+        break;
+      case 0b0010:  //! Casos de fazer mini direita 
+        vel_direita();
+        Serial.println(" / mini direita");
+        break;
+      case 0b1000:
+      case 0b1110:
+        Serial.println(" / 90 esq");
+        vel_parar(6000);
+        //esq_90();
+        break;
+      case 0b0001:
+      case 0b0111:
+        Serial.println(" / 90 dir");
+        //vel_parar(6000);
+        dir_90();
+        break;
+      default:
+        break;
+    } */
+  //delay(1000);
+//Serial.println();
+//delay(100);
+//vel_parar(1000);
 }
+
--- OBR 2024/cleber/definir.h
+++ OBR 2024/cleber/definir.h
@@ -14,18 +14,31 @@ Adafruit_TCS34725_SWwire tcs_soft = Adafruit_TCS34725_SWwire(TCS34725_INTEGRATIO
 // mpu6050 i2c = 0x68
 
 //* Definindo as portas dos sensores
-#define s_esq A4     //
-#define s_mesq A3 //
+#define s_esq A15     //
+#define s_mesq A2    //
 #define s_m A0     //
-#define s_mdir A2 //
-#define s_dir A1     //
+#define s_mdir A3 //
+#define s_dir A4     //
 
-// diminuir 20 do max
-#define max_esq 125
-#define max_mesq 170
-#define max_m 130
-#define max_mdir 230
-#define max_dir 200
+
+#define branco_esq 227
+#define branco_mesq 251
+#define branco_m 155
+#define branco_mdir 196
+#define branco_dir 137
+
+#define preto_esq 180
+#define preto_mesq 170
+#define preto_m 75
+#define preto_mdir 108
+#define preto_dir 86
+
+//Esquerda sendo branco e direita sendo preto
+#define media_esq (145 + 90) / 2
+#define media_mesq (192 + 110) / 2
+#define media_m (155 + 78) / 2
+#define media_mdir (245 + 156) / 2
+#define media_dir (212 + 160) / 2
 
 bool s_bit = false;
 byte leitura = 0;
@@ -35,9 +48,9 @@ bool ver = false;      // O Verifica para os switchs
 #define servo_direita 12
 Servo serv_esq;
 Servo serv_dir;
-#define delay_fre 300
+#define delay_fre 500
 #define delay_re 30
-#define delay_peq 30 
+#define delay_peq 1500 
 #define delay_pas 30
 
 #define velocidade_par 40  // Delay para o tempo dele ficar parado
@@ -58,7 +71,7 @@ Servo serv_dir;
  */
 Ultrasonic ult_meio(30, 31);
 
-void calibra()
+/* void calibra()
 {
   leitura = 0;
 
@@ -79,7 +92,7 @@ void calibra()
     s_bit = true;
   else
     s_bit = false;
-  leitura |= s_bit << 2;*/
+  leitura |= s_bit << 2;
 
   if (analogRead(s_mdir) >= max_mdir)
     s_bit = true;
@@ -94,29 +107,72 @@ void calibra()
   leitura |= s_bit << 3;
 
   leitura = (~leitura) & 0b00001111;  
+} */
+
+void calibra()
+{
+  leitura = 0;
+  s_bit = analogRead(s_esq) >= media_esq ? 1 : 0;
+  leitura |= s_bit << 0;
+
+  s_bit = analogRead(s_mesq) >= media_mesq ? 1 : 0;
+  leitura |= s_bit << 1; 
+
+  /* s_bit = analogRead(s_m) <= media_m ? 1 : 0;
+  leitura |= s_bit << 1; */
+
+  s_bit = analogRead(s_mdir) >= media_mdir ? 1 : 0;
+  leitura |= s_bit << 2;
+
+  s_bit = analogRead(s_dir) >= media_dir ? 1 : 0;
+  leitura |= s_bit << 3;
+
+  leitura = (~leitura) & 0b00001111;
 }
 
+/* 
+void calibra()
+{
+  leitura = 0;
+
+  s_bit = analogRead(s_esq) >= media_esq ? 1 : 0;
+  leitura |= s_bit << 0;
+
+  s_bit = analogRead(s_mesq) >= media_mesq ? 1 : 0;
+  leitura |= s_bit << 1; 
+
+  s_bit = analogRead(s_m) <= media_m ? 1 : 0;
+  leitura |= s_bit << 1; 
+
+  s_bit = analogRead(s_mdir) >= media_mdir ? 1 : 0;
+  leitura |= s_bit << 2;
+
+  s_bit = analogRead(s_dir) >= media_dir ? 1 : 0;
+  leitura |= s_bit << 3;
+
+  leitura = (~leitura) & 0b00001111;
+} */
 
 //* Inicio das funções, para cada caso
 void vel_frente()
 {
-  serv_esq.write(180);
-  serv_dir.write(0);
+  serv_esq.write(100);
+  serv_dir.write(80);
 }
 void vel_direita()
 {
-  serv_esq.write(180); // talvez usar 90
-  serv_dir.write(180);
+  serv_esq.write(100); // talvez usar 90
+  serv_dir.write(100);
 }
 void vel_esquerda()
 {
-  serv_esq.write(0);
-  serv_dir.write(0); // talvez usar 90
+  serv_esq.write(80);
+  serv_dir.write(80); // talvez usar 90
 }
 void vel_re()
 {
-  serv_esq.write(0);
-  serv_dir.write(180);
+  serv_esq.write(80);
+  serv_dir.write(100);
 }
 void vel_parar(int velo_esq = velocidade_par)
 {
@@ -125,134 +181,27 @@ void vel_parar(int velo_esq = velocidade_par)
   delay(velo_esq);
 }
 
-enum cores {
-  branco_branco, branco_verde,
-  verde_branco , verde_verde,
-  vermelho_vermelho,
-  outro
-};
-
-cores sensi()
-{
-  int cor1, cor2;
-  uint16_t r1, g1, b1, c1;
-  uint16_t r2, g2, b2, c2;
-
-  tcs_real.getRawData(&r1, &g1, &b1, &c1);
-  tcs_soft.getRawData(&r2, &g2, &b2, &c2);
-
-  uint16_t t1  = (r1+g1+b1)/3;
-  uint16_t tr1 = (g1+b1)/2;
-  uint16_t tg1 = (r1+b1)/2;
-
-  uint16_t t2  = (r2+g2+b2)/3;
-  uint16_t tr2 = (g2+b2)/2;
-  uint16_t tg2 = (r2+b2)/2;
-
-  if(r1 > tr1*1.07)
-    return vermelho_vermelho;
-  else if(g1 > tg1*1.07)
-    cor1 = 1; //verd
-  else {
-    if(t1 >= 11500) //1050
-      cor1 = 2; //b
-    else 
-      cor1 = 3; //p
-  }
-
-  if(r2 > tr2*1.07)
-    return vermelho_vermelho;
-  else if(g2 > tg2*1.07)
-    cor2 = 1;
-  else {
-    if(t2 >= 11500) //1050
-      cor2 = 2;
-    else 
-      cor2 = 3;
-  }
-
-  if     (cor1 == 1 && cor2 == 1)
-    return verde_verde;
-  else if(cor1 == 1 && cor2 == 2)
-    return branco_verde;
-  else if(cor1 == 2 && cor2 == 1)
-    return verde_branco;
-  else
-    return branco_branco;
-
-
-}
-
-/**
- *! false = direita
- *! true = esquerda
- */
-void desv()
-{
-  delay(delay_re); //* Dando um passo para atras, isso e bom caso a traseira do robo e maior do que na frente
-/*mot1_par();                                //* Colocando pra parar bem rapido pq sim
-mot2_par();
-delay(mot_par);*/
-#if desv_lado                                //! Esquerda
-  delay(enc_90);  //* Girando para esquerda
-  delay(frente_1);  //* Se distanciando do obstaculo
-  delay(enc_90_2); //* Virando para direita, com valor variado para nao girar demais
-  delay(frente_2);  //* Passando do obstaculo
-  delay(enc_90_3); //* Virando para direita, mesmo moitvo anterior
-  delay(frente_3);  //* Andando em frente, para ele nao se confundir linhas aleatorias
-  vel_frente();            //* Terminando com while para ele encontrar a linah correta
-  while ((digitalRead(s_mesq) == 1) && (digitalRead(s_mdir) == 1)); 
-  delay(delay_peq_desv); //* Se afastando um pouco da linha
-  vel_esquerda();             //* Virando para esquerda para se ajeiar na faixa
-  while ((digitalRead(s_mesq) == 1) && (digitalRead(s_mdir) == 1));
-#else                                         //! Direita
-  delay(enc_90);    //* Girando para direita
-  delay(frente_1);   //* Se distanciando do obstaculo
-  delay(enc_90_2); //* Virando para esquerda, com valor variado para nao girar demais
-  delay(frente_2);   //* Passando do obstaculo
-  delay(enc_90_3); //* Virando para esquerda, mesmo moitvo anterior
-  delay(frente_3);   //* Andando em frente, para ele nao se confundir linhas aleatorias
-  vel_frente(velo_esq, velo_dir);             //* Terminando com while para ele encontrar a linah correta
-  while ((digitalRead(s_mesq) == 1) && (digitalRead(s_mdir) == 1))
-  {
-  }
-  delay(enc_peq_desv); //* Se afastando um pouco da linha
-  vel_dir(velo_esq, velo_dir);                  //* Virando para direita para se ajeiar na faixa
-  while ((digitalRead(s_mesq) == 1) && (digitalRead(s_mdir) == 1))
-  {
-  }
-#endif
-}
-
 void esq_90() //* 90 esquerda
 {
   vel_frente();
   delay(delay_fre);
-
-  if (analogRead(s_m) >= max_m)
-  {
-    vel_esquerda();
-    delay(delay_peq);
-    while (((analogRead(s_mesq) >= max_mesq) || (analogRead(s_mdir) >= max_mdir)) && analogRead(s_esq) >= max_esq);
-    // while ((digitalRead(s_m) == 1) && (digitalRead(s_esq) == 1));
-    vel_re();
-    delay(delay_re);
-  }
+  vel_esquerda();
+  delay(delay_peq);
+  //while (((analogRead(s_mesq) <= media_mesq) || (analogRead(s_mdir) <= media_mdir)) && analogRead(s_esq) <= media_esq);
+  // while ((digitalRead(s_m) == 1) && (digitalRead(s_esq) == 1));
+  //vel_re();
+  //delay(delay_re);
 }
 
 void dir_90() //* 90 direita
 {
   vel_frente();
   delay(delay_fre);
-
-  if (analogRead(s_m) >= max_m)
-  {
-    vel_direita();
-    delay(delay_peq);
-    while (((analogRead(s_mesq) >= max_mesq) || (analogRead(s_mdir) >= max_mdir)) && analogRead(s_dir) >= max_dir);
-    // while ((digitalRead(s_m) == 1) && (digitalRead(s_dir) == 1));
-    vel_re();
-    delay(delay_re);
-  }
+  vel_direita();
+  delay(delay_peq);
+  //while ((analogRead(s_mesq) >= media_mesq) && (analogRead(s_mdir) >= media_mdir) /* && (analogRead(s_dir) <= media_dir) */);
+  // while ((digitalRead(s_m) == 1) && (digitalRead(s_dir) == 1));
+  //vel_re();
+  //delay(delay_re);
 }
 #endif
